22e7290d3fcab9d5edc468794a4e12c0f28c3a32
khoahuynhf <khoa0319@gmail.com>
2020-02-22 13:31:21 +0700

    sqrt with Newton's method

--- 1.rkt
+++ 1.rkt
@@ -1,5 +1,24 @@
-#lang planet neil/sicp
-(define (sqrt x)
+#lang racket
+
+(define (square x)
   (* x x))
 
-/* (sqrt 5) */
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- x (square guess)))
+    0.00001))
+
+(define (try guess x)
+  (if (good-enough? guess x)
+    guess
+    (try (improve guess x) x)))
+
+(define (sqrt x)
+  (try 1.0 x))
+
+(sqrt 100.0)
