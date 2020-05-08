21b19ffba02fcb4abdc941a8dfd594f7274d9f1c
khoahuynhf <khoa0319@gmail.com>
2020-05-08 16:56:10 +0700

    pretty print pascal triangle

--- ex1.12.scm
+++ ex1.12.scm
@@ -7,10 +7,27 @@
 ; compute element of pascal triangle
 
 (define (pascal r c)
-  (if (or (= r 1) (= c 1) (= r c))
+  (if (or (= c 1) (= r c))
       1
       (+ (pascal (- r 1) (- c 1))
 	 (pascal (- r 1) c))))
 
 ; (pascal 3 2) ;2
-(pascal 5 3) ; 6
+; (pascal 5 3) ; 6
+
+; pretty print it
+(define (print-pascal-triangle depth)
+  (define (iter-row n)
+    (define (iter-col r c)
+      (display (pascal r c))
+      (display " ")
+      (if (= r c)
+	  (newline)
+	  (iter-col r (+ c 1))))
+    (iter-col n 1)
+    (if (= n depth)
+	(values)
+	(iter-row (+ n 1))))
+  (iter-row 1))
+
+(print-pascal-triangle 20)
